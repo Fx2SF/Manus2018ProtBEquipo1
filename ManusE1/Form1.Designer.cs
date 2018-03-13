316c2d8f6b71d0059a951f218ff98aba2d53c787
Fausto Márquez <fausto_7m@hotmail.com>
2018-03-12 23:09:47 -0300

    Prueba push

---
+++ ManusE1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ManusE1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.processButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 88);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Examinar";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // processButton
+            // 
+            this.processButton.Enabled = false;
+            this.processButton.Location = new System.Drawing.Point(203, 88);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(75, 23);
+            this.processButton.TabIndex = 1;
+            this.processButton.Text = "Procesar";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 127);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ManusE1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using Google.Cloud.Vision.V1;
+using ImageProcessor;
+using ImageProcessor.Imaging.Filters.Photo;
+using System.Drawing;
+namespace ManusE1
+{
+    public partial class Form1 : Form
+    {
+        string fotoPath;
+        string txtPath;
+        string directory;
+        Stream fotoStream;
+        string text = "";
+        //Creo instancia de cliente API
+        ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+            private void processButton_Click(object sender, EventArgs e) {
+                textBox1.Text = "Procesando...";
+
+                //Creo archivo txt 
+                CreateTXT(directory);
+
+                //Creo rectángulos de recortes que interesan para procesar
+                Rectangle z1 = new Rectangle(3, 3, 3, 3);
+                Rectangle z2 = new Rectangle(3, 3, 3, 3);
+                Rectangle z3 = new Rectangle(3, 3, 3, 3);
+                Rectangle[] cropZones = new Rectangle[] { z1, z2, z3 };
+                foreach (Rectangle z in cropZones) {
+                    //crop this zone in image, create tiff file with it n process it
+                }
+
+                textBox1.Text = "Se creó un txt con el resultado.";
+                processButton.Enabled = false;
+                
+            }
+
+            private void openButton_Click(object sender, EventArgs e) {
+                // Consigo path de imagen 
+                fotoStream = null;
+                openFileDialog1.InitialDirectory = "c:\\" ;
+                openFileDialog1.Filter = "Archivos jpg (*.jpg)|*.jpg*|Todos los archivos (*.*)|*.*" ;
+                openFileDialog1.FilterIndex = 2 ;
+                openFileDialog1.RestoreDirectory = true ;
+
+                if(openFileDialog1.ShowDialog() == DialogResult.OK) {
+                    fotoStream = openFileDialog1.OpenFile();
+                    fotoPath = openFileDialog1.FileName;
+                    directory = Path.GetDirectoryName(fotoPath);
+                }
+                textBox1.Text = ("Cargando imagen...");
+                //improveImage(fotoStream, directory);
+                textBox1.Text = ("Imagen cargada. Listo para procesar.");
+                processButton.Enabled = true;
+            }
+
+            public void CreateTXT(string directory) {
+                txtPath = string.Concat(directory, "\\Texto Resultado.txt"); //Creo el archivo con nombre "Texto Resultado"
+                if (File.Exists(txtPath)) {
+                    textBox1.Text = "Ya existía otro archivo de texto resultado. Se reemplazó.";
+                }
+                FileStream file = File.Create(txtPath);
+                file.Dispose(); //Elimino el objeto para que writeTXT pueda usar el archivo creado sin que este proceso lo tenga abierto.
+            }
+
+            public void writeTXT(string txtFilePath, string text) {
+                StreamWriter file = new StreamWriter(txtFilePath);
+                file.Write(text);
+                file.Dispose();
+            }
+
+            public void improveImage(Stream img, string directory) {
+                var imageFactory = new ImageFactory(false);
+                var improvedImg = imageFactory.Load(img);
+                //Aplico mejoras
+                improvedImg.Filter(MatrixFilters.BlackWhite);
+                improvedImg.Contrast(70);
+                improvedImg.AutoRotate();
+
+                //Guardo imagen mejorada
+                improvedImg.Save(string.Concat(directory, "\\improvedImg.jpg"));
+            }
+
+            public void cropAndProcessImage(string img) {
+
+            }
+
+            public void processSingleCrop() {
+                //Pido a la API Vision
+                var image = Image.FromFile(fotoPath);
+                var response = client.DetectText(image);
+                
+                //Escribo lo devuelto por Vision 
+                text = response.ElementAt(0).Description;
+                writeTXT(txtPath, text);
+                //Elimino imagen creada por improveImage en directorio
+                File.Delete(string.Concat(directory, "\\improvedImg.jpg"));
+            }
+
+        }
+    }
